3b69f0b40f3e8914b2112815832ec6cda23fbc43
Raven Iqqe <raviqqe@gmail.com>
2015-12-30 14:26:20 +0900

    Make it work with bracketed expressions

--- lambda.py
+++ lambda.py
@@ -8,7 +8,8 @@ import unicodedata
 
 # constants
 
-PUNCTUATIONS = {"\\", "."}
+PUNCTUATIONS = {"\\", ".", "(", ")"}
+DEBUG = True
 
 
 
@@ -17,14 +18,22 @@ PUNCTUATIONS = {"\\", "."}
 ## error
 
 class Error:
-  def __init__(self, message):
-    self.message = message
+  def __init__(self, pos, message, raw_message=False):
+    if not raw_message:
+      self.message = self.format_message(pos, message)
+    else:
+      self.message = message
 
   def __str__(self):
     return self.message
 
-  def append_message(self, message):
-    return Error(message + "\n" + self.message)
+  def append_message(self, pos, message):
+    return Error(pos, self.message + "\n" + self.format_message(pos, message),
+                 raw_message=True)
+
+  @staticmethod
+  def format_message(pos, message):
+    return "lambda:" + str(pos) + ": " + message
 
 
 ## AST
@@ -49,7 +58,7 @@ class Variable(AstNode):
   def eval(self, env):
     if self.name in env:
       return env[self.name]
-    return Error("A variable, \"{}\" is not defined.".format(self.name))
+    return self
 
 
 class LambdaAbstraction(AstNode):
@@ -95,53 +104,91 @@ class Parser:
     return result
 
   def expression(self):
-    return self.term()
+    def expression_parser(old_pos):
+      result, pos = choice(self.term(),
+                           self.bracketed(recursed(self.expression)))(old_pos)
+      if isinstance(result, Error):
+        debug_parser(old_pos, "expression failed.")
+        return Error(old_pos, "An expression is expected."), old_pos
+      debug_parser(pos, "expression parsed.")
+      return result, pos
+    return expression_parser
 
   def term(self):
-    def term_parser(pos):
+    def term_parser(old_pos):
       result, pos = choice(self.variable(),
                            self.lambda_abstraction(),
-                           self.function_application())(pos)
+                           self.function_application())(old_pos)
       if isinstance(result, Error):
-        return result.append_message("A term is expected."), pos
+        debug_parser(old_pos, "term failed.")
+        return result.append_message(old_pos, "A term is expected."), old_pos
+      debug_parser(pos, "term parsed.")
       return result, pos
     return term_parser
 
+  def bracketed(self, parser):
+    def bracketed_parser(old_pos):
+      results, pos = sequence(self.punctuation("("),
+                              parser,
+                              self.punctuation(")"))(old_pos)
+      if isinstance(results, Error):
+        debug("bracketed failed.")
+        return results, old_pos
+      debug("bracketed parsed.")
+      return results[1], pos
+    return bracketed_parser
+
   def variable(self):
-    def variable_parser(pos):
-      result, pos = self.identifier()(pos)
+    def variable_parser(old_pos):
+      result, pos = self.identifier()(old_pos)
       if isinstance(result, Error):
-        return result.append_message("A variable is expected."), pos
+        debug_parser(old_pos, "variable failed.")
+        return result.append_message(old_pos, "A variable is expected."), \
+               old_pos
+      debug_parser(pos, "variable parsed.")
       return Variable(result), pos
 
     return variable_parser
 
   def lambda_abstraction(self):
-    def lambda_abstraction_parser(pos):
+    def lambda_abstraction_parser(old_pos):
       results, pos = sequence(self.punctuation("\\"),
                               self.identifier(),
-                              self.punctuation("."))(pos)
+                              self.punctuation("."))(old_pos)
       if isinstance(results, Error):
-        return results.append_message("A lambda abstraction is expected."), pos
+        debug("lambda abstraction failed.")
+        return results.append_message(old_pos,
+                                      "A lambda abstraction is expected."), \
+               old_pos
 
       result, pos = self.expression()(pos)
       if isinstance(result, Error):
-        return result.append_message("An expression is expected."), pos
+        debug("lambda abstraction failed.")
+        return result.append_message(old_pos, "An expression is expected."), \
+               old_pos
 
+      debug("lambda abstraction parsed.")
       return LambdaAbstraction(results[1], result), pos
 
     return lambda_abstraction_parser
 
   def function_application(self):
-    def function_application_parser(pos):
-      result_1, pos = self.expression()(pos)
+    def function_application_parser(old_pos):
+      result_1, pos = choice(self.variable(),
+                             self.lambda_abstraction(),
+                             self.bracketed(self.expression()))(old_pos)
       if isinstance(result_1, Error):
-        return result_1.append_message("An expression is expected."), pos
+        debug("function application failed.")
+        return result_1.append_message(old_pos, "An expression is expected."),\
+               old_pos
 
       result_2, pos = self.expression()(pos)
       if isinstance(result_2, Error):
-        return result_2.append_message("An expression is expected."), pos
+        debug("function application failed.")
+        return result_2.append_message(old_pos, "An expression is expected."),\
+               old_pos
 
+      debug("function application parsed.")
       return FunctionApplication(result_1, result_2), pos
 
     return function_application_parser
@@ -151,25 +198,32 @@ class Parser:
       _, pos = self.blanks()(old_pos)
       results, pos = sequence(self.letter(), many(self.letter()))(pos)
       if isinstance(results, Error):
-        return Error("An identifier is expected."), old_pos
-      return results[0] + "".join(results[1]), pos
+        debug_parser(old_pos, "identifier failed.")
+        return Error(old_pos, "An identifier is expected."), old_pos
+      identifier = results[0] + "".join(results[1])
+      debug_parser(pos, "identifier, \"{}\" parsed.".format(identifier))
+      return identifier, pos
 
     return identifier_parser
 
   def punctuation(self, punctuation):
     assert punctuation in PUNCTUATIONS
 
-    def punctuation_parser(pos):
-      _, pos = self.blanks()(pos)
+    def punctuation_parser(old_pos):
+      _, pos = self.blanks()(old_pos)
       if self.text[pos:pos+len(punctuation)] == punctuation:
+        debug("punctuation, {} parsed.".format(punctuation))
         return punctuation, pos + len(punctuation)
-      return Error("A punctuation, \"{}\" is expected.".format(punctuation)), \
-             pos
+      debug("punctuation, {} failed.".format(punctuation))
+      return Error(old_pos,
+                   "A punctuation, \"{}\" is expected.".format(punctuation)), \
+             old_pos
 
     return punctuation_parser
 
   def blanks(self):
-    def blanks_parser(pos):
+    def blanks_parser(old_pos):
+      pos = old_pos
       while pos < len(self.text) and self.text[pos] in {" ", "\t", "\n"}:
         pos += 1
       return None, pos
@@ -177,11 +231,12 @@ class Parser:
     return blanks_parser
 
   def letter(self):
-    def letter_parser(pos):
+    def letter_parser(old_pos):
+      pos = old_pos
       if len(self.text[pos:]) > 0 \
          and unicodedata.category(self.text[pos]).startswith("L"):
         return self.text[pos], pos + 1
-      return Error("A letter is expected."), pos
+      return Error(old_pos, "A letter is expected."), old_pos
 
     return letter_parser
 
@@ -190,12 +245,12 @@ class Parser:
 # functions
 
 def choice(*parsers):
-  def choice_parser(pos):
+  def choice_parser(old_pos):
     for parser in parsers:
-      result, pos = parser(pos)
+      result, pos = parser(old_pos)
       if not isinstance(result, Error):
         return result, pos
-    return result, pos
+    return Error(old_pos, "A choice parser failed."), old_pos
   return choice_parser
 
 
@@ -204,17 +259,17 @@ def sequence(*parsers):
     pos = old_pos
     results = []
     for parser in parsers:
-      result, new_pos = parser(pos)
+      result, pos = parser(pos)
       if isinstance(result, Error):
-        return result, pos
-      pos = new_pos
+        return result, old_pos
       results.append(result)
     return results, pos
   return sequence_parser
 
 
 def many(parser):
-  def many_parser(pos):
+  def many_parser(old_pos):
+    pos = old_pos
     results = []
     result, new_pos = parser(pos)
     while not isinstance(result, Error):
@@ -226,19 +281,32 @@ def many(parser):
 
 
 def recursed(parser_generator, *arguments):
-  def recursed_parser(pos):
-    return parser_generator(*arguments)(pos)
+  def recursed_parser(old_pos):
+    return parser_generator(*arguments)(old_pos)
   return recursed_parser
 
 
 def interpret(text):
-  return Parser().parse(text).eval({})
+  result = Parser().parse(text)
+  if isinstance(result, Error):
+    return result
+  return result.eval({})
 
 
 ## utils
 
 def usage():
   exit("usage: {} [<file>]".format(sys.argv[0]))
+
+
+def debug(*messages):
+  if DEBUG:
+    print(*messages, file=sys.stderr)
+
+
+def debug_parser(pos, *messages):
+  if DEBUG:
+    print(str(pos) + ":", *messages, file=sys.stderr)
 
 
 
